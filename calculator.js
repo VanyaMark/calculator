d48616477863bc001ca8a6360f8acb2a9d794f7e
VanyaMark <markova.vanya@gmail.com>
2024-08-26 23:57:58 +0200

    logic more or less done

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 let runningTotal = 0;
 let buffer = "0";
-let previousOperator;
+let previousOperator = null;
 
 
 const screen = document.querySelector('.screen');
@@ -17,9 +17,65 @@ function buttonClick(value) {
 }
 
 function handleSymbol(symbol) {
-    if (symbol === 'C') {
-        buffer = 0;
-        runningTotal = 0;
+    switch(symbol) {
+        case 'C':
+            buffer = '0';
+            runningTotal = 0;
+            break;
+        case '=':
+            if (previousOperator === null) {
+                // need 2 numbers to do math
+                return;
+            } 
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = runningTotal;
+            runningTotal = 0;
+            break;
+        case '←':
+            if (buffer.length === 1) {
+                buffer = '0';
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1)
+            }
+            break;
+        case '+':
+        case '-':
+        case '×':
+        case '÷':
+            handleMath(symbol);
+            break;
+    }
+}
+
+function handleMath(symbol) {
+    console.log(symbol)
+    if (buffer === '0') {
+        //do nothing
+        return
+    }
+    const intBuffer = parseInt(buffer);
+
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = symbol;
+
+    buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+    if (previousOperator === '+') {
+        runningTotal += intBuffer;
+    } else if (previousOperator === '-') {
+        runningTotal -= intBuffer;
+    } else if (previousOperator === '×') {
+        runningTotal *= intBuffer;
+    } else {
+        runningTotal /= intBuffer;
     }
 }
 
